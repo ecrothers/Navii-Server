61a1096d4e6f730284d53dadf79707b1e8ef2b81
EvanC <evan.crothers@gmail.com>
2015-11-07 22:48:37 -0500

    un-adding webstorm project

---
+++ gs-rest-service-master/complete/src/main/java/com/navii/server/controller/WebtoolController.java
@@ -0,0 +1,26 @@
+package com.navii.server.controller;
+
+import com.navii.server.domain.Itinerary;
+import com.navii.server.service.ItineraryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/itinerary")
+public class WebtoolController {
+    private static final Logger logger = LoggerFactory.getLogger(WebtoolController.class);
+
+    @Autowired
+    private ItineraryService itineraryService;
+
+    @RequestMapping(value="/", method= RequestMethod.POST)
+    public Itinerary createItinerary(@RequestBody Itinerary itinerary) {
+        Itinerary created = itineraryService.save(itinerary);
+        return created;
+    }
+}
