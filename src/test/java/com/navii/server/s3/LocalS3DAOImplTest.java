99e48f718c2aa83961890f3d569d314298786fe8
JMtorii <j.torii4@gmail.com>
2015-11-16 21:28:00 -0500

    Refactored for cleaner duplicate code

--- src/test/java/com/navii/server/s3/LocalS3DAOImplTest.java
+++ src/test/java/com/navii/server/s3/LocalS3DAOImplTest.java
@@ -1,67 +1,28 @@
 package com.navii.server.s3;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.navii.server.util.ObjectMapperFactory;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
 public class LocalS3DAOImplTest {
-
     private ObjectMapper objectMapper;
-
     private LocalS3DAOImpl localS3Dao;
-
-    private SomeTestObject someTestObject;
 
     @Before
     public void setup() {
         objectMapper = ObjectMapperFactory.createMapper();
         localS3Dao = new LocalS3DAOImpl(objectMapper, "ci");
         localS3Dao.setup();
-
-        someTestObject = new SomeTestObject("foo", "bar");
     }
 
     @Test
     public void saveAndLoadTest() {
-        String objId = "123";
-        localS3Dao.save(objId, someTestObject);
-
-        SomeTestObject fetchedObject = localS3Dao.load(SomeTestObject.class, objId);
-        assertEquals(someTestObject.key, fetchedObject.key);
-        assertEquals(someTestObject.value, fetchedObject.value);
+        S3TestHelper.saveAndLoadTest(localS3Dao, "foo", "bar");
     }
 
     @Test
     public void deleteTest() {
-        String objId = "123";
-        localS3Dao.delete(someTestObject.getClass(), objId);
-
-        SomeTestObject shouldBeDeleted = localS3Dao.load(someTestObject.getClass(), objId);
-        assertNull(shouldBeDeleted);
-    }
-
-    @S3DocumentType("SomeTestObject")
-    private static class SomeTestObject {
-        @JsonProperty
-        String key;
-
-        @JsonProperty
-        String value;
-
-        /**
-         * For Jackson
-         */
-        @SuppressWarnings("unused")
-        public SomeTestObject() {}
-
-        public SomeTestObject(String key, String value) {
-            this.key = key;
-            this.value = value;
-        }
+        S3TestHelper.deleteTest(localS3Dao, "foo", "bar");
     }
 }
--- src/test/java/com/navii/server/s3/S3DAOImplTest.java
+++ src/test/java/com/navii/server/s3/S3DAOImplTest.java
@@ -1,13 +1,10 @@
 package com.navii.server.s3;
 
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.navii.server.util.ObjectMapperFactory;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Created by JMtorii on 2015-11-15.
@@ -15,7 +12,6 @@ import static org.junit.Assert.assertEquals;
 public class S3DAOImplTest {
     private ObjectMapper objectMapper;
     private S3DAOImpl s3DAO;
-    private SomeTestObject someTestObject;
 
     @Before
     public void setup() {
@@ -23,37 +19,10 @@ public class S3DAOImplTest {
         AmazonS3Client s3Client = new AmazonS3Client();
         s3DAO = new S3DAOImpl(objectMapper, s3Client, "navi.ci.test", false, "ci");
         s3DAO.setup();
-
-        someTestObject = new SomeTestObject("foo", "bar");
     }
 
     @Test
     public void saveAndLoadTest() {
-        String objId = "123";
-        s3DAO.save(objId, someTestObject);
-
-        SomeTestObject fetchedObject = s3DAO.load(SomeTestObject.class, objId);
-        assertEquals(someTestObject.key, fetchedObject.key);
-        assertEquals(someTestObject.value, fetchedObject.value);
-    }
-
-    @S3DocumentType("SomeTestObject")
-    private static class SomeTestObject {
-        @JsonProperty
-        String key;
-
-        @JsonProperty
-        String value;
-
-        /**
-         * For Jackson
-         */
-        @SuppressWarnings("unused")
-        public SomeTestObject() {}
-
-        public SomeTestObject(String key, String value) {
-            this.key = key;
-            this.value = value;
-        }
+        S3TestHelper.saveAndLoadTest(s3DAO, "foo", "bar");
     }
 }
