fdebd09ad876ce0783b90bb05a43ead8a3407e1e
jsh0211@gmail.com <jsh0211@gmail.com>
2016-04-09 13:41:57 -0400

    Adding database drop scripts

---
+++ sql/dropscripts.sql
@@ -0,0 +1,24 @@
+
+Use naviDB;
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS attractions;
+DROP TABLE IF EXISTS itineraries;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS cuisines;
+DROP TABLE IF EXISTS atmospheres;
+DROP TABLE IF EXISTS diets;
+DROP TABLE IF EXISTS types;
+DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS pointofinterests;
+DROP TABLE IF EXISTS preferences;
+DROP TABLE IF EXISTS preference_questions;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS userspreferences;
+DROP TABLE IF EXISTS attractionspreferences;
+DROP TABLE IF EXISTS attractionstags;
+DROP TABLE IF EXISTS itinerariespreferences;
+DROP TABLE IF EXISTS itinereariestags;
+
+SET FOREIGN_KEY_CHECKS = 1;
