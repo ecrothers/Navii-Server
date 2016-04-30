ca2cc42f913be17d311f809e007676124002b829
jsh0211 <jsh0211@gmail.com>
2016-04-30 16:06:28 -0400

    Initial integration of Yelp API (direct code copy)

---
+++ src/main/java/com/navii/server/persistence/yelpAPI/TwoStepOAuth.java
@@ -0,0 +1,28 @@
+package com.navii.server.persistence.yelpAPI;
+
+/**
+ * Created by sjung on 30/04/16.
+ */
+import org.scribe.builder.api.DefaultApi10a;
+import org.scribe.model.Token;
+
+/**
+ * Generic service provider for two-step OAuth10a.
+ */
+public class TwoStepOAuth extends DefaultApi10a {
+
+    @Override
+    public String getAccessTokenEndpoint() {
+        return null;
+    }
+
+    @Override
+    public String getAuthorizationUrl(Token arg0) {
+        return null;
+    }
+
+    @Override
+    public String getRequestTokenEndpoint() {
+        return null;
+    }
+}
