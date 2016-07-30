ceb38b9a443bffc935b20f10a7b512fbfaaa0e10
jsh0211 <jsh0211@gmail.com>
2016-07-30 19:21:58 -0400

    Jenkins test 2

--- sql/insert_test_data_scripts.sql
+++ sql/insert_test_data_scripts.sql
@@ -2,22 +2,22 @@ INSERT INTO preference_questions (preference_type, preference_question) VALUES (
 INSERT INTO preference_questions (preference_type, preference_question) VALUES (2, "What is your personal vibe?");
 INSERT INTO preference_questions (preference_type, preference_question) VALUES (3, "What are your dining preferences?");
 
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('gluten-free', 1);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('vegan', 1);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('vegetarian', 1);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('halal', 1);
+INSERT INTO preferences (preference, preference_type) VALUES ('gluten-free', 1);
+INSERT INTO preferences (preference, preference_type) VALUES ('vegan', 1);
+INSERT INTO preferences (preference, preference_type) VALUES ('vegetarian', 1);
+INSERT INTO preferences (preference, preference_type) VALUES ('halal', 1);
 
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('adventure', 2);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('outdoor', 2);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('adult', 2);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('foodie', 2);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('sporty', 2);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('lazy', 2);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('cultural', 2);
+INSERT INTO preferences (preference, preference_type) VALUES ('adventure', 2);
+INSERT INTO preferences (preference, preference_type) VALUES ('outdoor', 2);
+INSERT INTO preferences (preference, preference_type) VALUES ('adult', 2);
+INSERT INTO preferences (preference, preference_type) VALUES ('foodie', 2);
+INSERT INTO preferences (preference, preference_type) VALUES ('sporty', 2);
+INSERT INTO preferences (preference, preference_type) VALUES ('lazy', 2);
+INSERT INTO preferences (preference, preference_type) VALUES ('cultural', 2);
 
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('hipster', 3);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('sophisticated', 3);
-INSERT INTO preferences (preference, photoURL, preference_type) VALUES ('cheap', 3);
+INSERT INTO preferences (preference, preference_type) VALUES ('hipster', 3);
+INSERT INTO preferences (preference, preference_type) VALUES ('sophisticated', 3);
+INSERT INTO preferences (preference, preference_type) VALUES ('cheap', 3);
 
 INSERT INTO tags (tag) VALUES 
 ('adventurous'),
